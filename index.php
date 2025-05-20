f568d534ec5c75b3cc9e2a3aebee6a9cf7f00331
GuiiAnanias <guiiananias@hotmail.com>
2025-05-20 20:14:08 -0300

    index.php concluido

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include 'conexao.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogo de livros</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Catálogo de livros</h1>
+    <a href="add.php" class="botao">+adicionar livros</a>
+
+    <table>
+            <tr>
+                <th>Titulo</th>
+                <th>Autor</th>
+                <th>Açôes</th>
+            </tr>
+            <?php 
+            $sql = "SELECT * FROM livros";
+            $result = $con->query($sql);
+
+            if ($result->num_rows > 0):
+                while($livro = $result->fetch_assoc()):
+            ?>
+                <tr>
+                    <td><?= htmlspecialchars($livro['titulo']) ?></td>
+                    <td><?= htmlspecialchars($livro['autor']) ?></td>
+                    <td>
+                        <a href="edit.php?id=<?= $livro['id'] ?>">Editar</a> |
+                        <a href="delete.php?id=<?= $livro['id'] ?>" onclick="return confirm('Tem certeza?')">Remover</a>
+                    </td>
+                </tr>
+            <?php endwhile; else: ?>
+                <tr>
+                    <td colspan="3">Nenhum livro cadastrado.</td>
+                </tr>
+            <?php endif; ?>
+    </table>
+    </div>
+</body>
+</html>
+
